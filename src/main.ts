548e0c6dec240d63da82d21935d9db8efd8b7cb9
byteshouse <a.lihtgolt@gmail.com>
2025-05-25 20:01:41 +0300

    open cors

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,14 @@ async function bootstrap() {
     const port = process.env.SERVICE_PORT || 3000;
     const app = await NestFactory.create(AppModule);
 
-    app.enableCors({
-      origin: process.env.CORS_ORIGIN_HOST.split(',').map((host) =>
-        host.startsWith('http') ? host : new RegExp(host),
-      ),
-      methods: ['GET', 'PATCH', 'POST', 'DELETE'],
-      maxAge: 600,
-    });
+    // app.enableCors({
+    //   origin: process.env.CORS_ORIGIN_HOST.split(',').map((host) =>
+    //     host.startsWith('http') ? host : new RegExp(host),
+    //   ),
+    //   methods: ['GET', 'PATCH', 'POST', 'DELETE'],
+    //   maxAge: 600,
+    // });
+    app.enableCors({ origin: '*' });
 
     await app.listen(port);
   }
